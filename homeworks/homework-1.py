854c874dcf8e9049419529cd3963148aeabb5fb7
Maybrit S. Ortiz <maybritsortiz@visitor-10-56-119-237.wifi.berkeley.edu>
2024-09-25 15:51:36 -0700

    Homework #1 python file added

---
+++ homeworks/homework-1.py
@@ -0,0 +1,12 @@
+# A "heinous" example of arithemtic math problem is the following"
+print((5*4*3*2*1)/(3*2*1)*5-12(10-9)/2+1)
+# The answer should be 95
+# Seemingly simple arithemtic problem requires PEMDAS
+# I would see problems like this on the internet, it ALWAYS causes a stir
+# Use PEDMAS and go left and right, one wrong move can be apocalyptic...
+
+# What is the process to run this python file?
+# cd introduction-to-python/homeworks (This pathway takes me to the directory (homeworks).)
+# do nano "homework-1.py" (create python file and it is named homework-1.py)
+# Insert comments/print, save the file, and close it to return to the command line
+# python3 homework-1.py (this runs the python file.)
